0a9dd851e7080ce1ef84944adcb422741fbaa095
Graham Bruce <grahambruce84@gmail.com>
2017-06-10 21:56:10 +0100

    Updated CSS file

--- controller/adoptions_controller.rb
+++ controller/adoptions_controller.rb
@@ -5,10 +5,16 @@ require_relative '../models/adoption'
 require_relative '../models/animal'
 require_relative '../models/owner'
 
-get 'adoptions' do
-  @animals = Biting.all()
+get '/adoptions' do
+  @animals = Animal.all()
   @owners = Owner.all()
   erb(:"adoptions/index")
+end
+
+get 'adoption/new' do
+  @animals = Animal.all()
+  @owners = Owner.all()
+  erb(:"adoptions/new")
 end
 
 post '/adoptions' do
